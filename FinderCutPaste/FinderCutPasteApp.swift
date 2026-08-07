ca11899b50f09a237508006ca1eb6a859dba4f91
Kyle Yang <REmpire.dev@proton.me>
2026-08-07 14:09:54 -0600

    Initial Commit

---
+++ FinderCutPaste/FinderCutPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderCutPasteApp.swift
+//  FinderCutPaste
+//
+//  Created by Kyle Y on 2026-08-07.
+//
+
+import SwiftUI
+
+@main
+struct FinderCutPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinderCutPasteTests/FinderCutPasteTests.swift
@@ -0,0 +1,19 @@
+//
+//  FinderCutPasteTests.swift
+//  FinderCutPasteTests
+//
+//  Created by Kyle Y on 2026-08-07.
+//
+
+import Testing
+@testable import FinderCutPaste
+
+struct FinderCutPasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
